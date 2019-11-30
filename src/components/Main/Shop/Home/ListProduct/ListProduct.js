b24330c7cb360256121245b125cb703c65742776
Sora DBH <47997682+duonghuan2122000@users.noreply.github.com>
2019-11-30 08:47:40 +0700

    add ProductDetail UI

--- src/components/Main/Shop/Home/ListProduct/ListProduct.js
+++ src/components/Main/Shop/Home/ListProduct/ListProduct.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { SafeAreaView, Text, FlatList, TouchableOpacity, Image, Dimensions } from 'react-native';
+import { SafeAreaView, Text, FlatList, TouchableOpacity, Image, Dimensions, StyleSheet } from 'react-native';
 
 import backIcon from '../../../../../assets/icons/backList.png';
 import sp1 from '../../../../../assets/images/sp1.jpg';
@@ -10,16 +10,35 @@ const imageProductHeight = (height / 11 * 10 - 100) / 3 - 40,
     colorViewWidth = width - 30 - imageProductWidth - 10;
 
 export default class ListProduct extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+        this.goBack = this.goBack.bind(this);
+    }
+
+    goBack() {
+        const { navigation } = this.props;
+        navigation.goBack();
+    }
+
     render() {
+        const {
+            container, wrapper, header, iconHeader, titleHeader,
+            containerProduct, imageProduct, infoProduct, txtProductPrice,
+            viewColor
+        } = styles;
         return (
-            <SafeAreaView style={{ flex: 1, backgroundColor: '#C5C7C6', padding: 10 }}>
-                <SafeAreaView style={{backgroundColor: '#fff', shadowColor: '#000', shadowOffset: {width: 0, height: 3}, shadowOpacity: 0.2, elevation: 5, flex: 1}}>
-                    <SafeAreaView style={{ justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center', paddingHorizontal: 15, paddingVertical: 10 }}>
-                        <TouchableOpacity>
-                            <Image source={backIcon} style={{ height: 24, width: 24 }} />
+            <SafeAreaView style={container}>
+                <SafeAreaView style={wrapper}>
+                    <SafeAreaView style={header}>
+                        <TouchableOpacity onPress={this.goBack}>
+                            <Image source={backIcon} style={iconHeader} />
                         </TouchableOpacity>
-                        <Text style={{ color: '#e04696' }}>Party Dress</Text>
-                        <SafeAreaView style={{width: 15}} />
+                        <Text style={titleHeader}>Party Dress</Text>
+                        <SafeAreaView style={{ width: 15 }} />
                     </SafeAreaView>
 
                     <FlatList
@@ -27,18 +46,16 @@ export default class ListProduct extends Component {
                         keyExtractor={item => item.toString()}
                         numColumns={1}
                         renderItem={({ item }) => (
-                            <TouchableOpacity style={{ flexDirection: 'row', borderTopColor: '#C5C7C6', borderTopWidth: 1, marginHorizontal: 15, paddingTop: 15, marginBottom: 15 }}>
+                            <TouchableOpacity style={containerProduct}>
                                 <SafeAreaView>
-                                    <Image source={sp1} style={{ height: imageProductHeight, width: imageProductWidth }} />
+                                    <Image source={sp1} style={imageProduct} />
                                 </SafeAreaView>
-                                <SafeAreaView style={{ justifyContent: 'space-between', paddingLeft: 10 }}>
+                                <SafeAreaView style={infoProduct}>
                                     <Text>Product Name</Text>
-                                    <Text style={{ color: 'red' }}>Product Price</Text>
+                                    <Text style={txtProductPrice}>Product Price</Text>
                                     <Text>Material Sink</Text>
-                                    <SafeAreaView style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: colorViewWidth }}>
-                                        <SafeAreaView>
-                                            <Text>Color RoyalBlue</Text>
-                                        </SafeAreaView>
+                                    <SafeAreaView style={viewColor}>
+                                        <Text>Color RoyalBlue</Text>
                                         <SafeAreaView style={{ backgroundColor: 'blue', height: 10, width: 10, borderRadius: 5 }} />
                                         <SafeAreaView />
 
@@ -52,3 +69,16 @@ export default class ListProduct extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: { flex: 1, backgroundColor: '#C5C7C6', padding: 10 },
+    wrapper: { backgroundColor: '#fff', shadowColor: '#000', shadowOffset: { width: 0, height: 3 }, shadowOpacity: 0.2, elevation: 5, flex: 1 },
+    header: { justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center', paddingHorizontal: 15, paddingVertical: 10 },
+    iconHeader: { height: 24, width: 24 },
+    titleHeader: { color: '#e04696' },
+    containerProduct: { flexDirection: 'row', borderTopColor: '#C5C7C6', borderTopWidth: 1, marginHorizontal: 15, paddingTop: 15, marginBottom: 15 },
+    imageProduct: { height: imageProductHeight, width: imageProductWidth },
+    infoProduct: { justifyContent: 'space-between', paddingLeft: 10 },
+    txtProductPrice: { color: 'red' },
+    viewColor: { flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: colorViewWidth }
+});
--- src/components/Main/Shop/Home/ProductDetail/ProductDetail.js
+++ src/components/Main/Shop/Home/ProductDetail/ProductDetail.js
@@ -1,12 +1,107 @@
 import React, { Component } from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import { SafeAreaView, Text, ScrollView, Image, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+
+import icBack from '../../../../../assets/icons/back.png';
+import icLogo from '../../../../../assets/icons/cartfull.png';
+
+import sp1 from '../../../../../assets/images/sp1.jpg';
+
+const { height } = Dimensions.get('window');
+const heightImageProduct = height / 2.8,
+    widthImageProduct = heightImageProduct / 452 * 361;
 
 export default class ProductDetail extends Component {
     render() {
+        const {
+            iconHeader, container, wrapper, header,
+            imageProduct, wrapperImage, wrapperTitleProduct,
+            txtProductName, txtProductPrice, txtProductDescription,
+            wrapperInfo, txtInfo
+        } = styles;
         return (
-            <SafeAreaView>
-                <Text>ProductDetail</Text>
+            <SafeAreaView style={container}>
+                <SafeAreaView style={wrapper}>
+                    <SafeAreaView style={header}>
+                        <TouchableOpacity>
+                            <Image source={icBack} style={iconHeader} />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Image source={icLogo} style={iconHeader} />
+                        </TouchableOpacity>
+                    </SafeAreaView>
+                    <ScrollView>
+                        <ScrollView contentContainerStyle={wrapperImage} horizontal>
+                            <Image source={sp1} style={imageProduct} />
+                            <Image source={sp1} style={imageProduct} />
+                        </ScrollView>
+                        <SafeAreaView style={wrapperTitleProduct}>
+                            <Text style={txtProductName}>Product Name</Text>
+                            <Text style={txtProductPrice}> / Product Price</Text>
+                        </SafeAreaView>
+                        <SafeAreaView style={wrapperInfo}>
+                            <Text style={txtInfo}>Material Sink</Text>
+                            <SafeAreaView style={{flexDirection: 'row', alignItems: 'center'}}>
+                                <Text style={txtInfo}>Color Khaki</Text>
+                                <SafeAreaView style={{backgroundColor: 'green', borderRadius: 5, height: 10, width: 10, marginLeft: 10}} />
+                            </SafeAreaView>
+                        </SafeAreaView>
+                        <Text style={txtProductDescription}>Lorem Ipsum is simply dummy text of the printing and 
+                            tting industry. Lorem Ipsum has been the industry's standard 
+                            dummy text ever since the 1500s, when an unknown printer took 
+                            alley of type and scrambled it to make a type specimen book. It 
+                            has survived not only five centuries, but also the leap into 
+                            electronic typesetting, remaining essentially unchanged. It 
+                            was popularised in the 1960s with the release of Letraset sheets 
+                            aining Lorem Ipsum passages, and more recently with desktop 
+                            publishing software like Aldus PageMaker including versions 
+                            of Lorem Ipsum.</Text>
+                    </ScrollView>
+                </SafeAreaView>
             </SafeAreaView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    iconHeader: { height: 24, width: 24 },
+    container: { flex: 1, backgroundColor: '#c5c7c6', padding: 15 },
+    wrapper: { flex: 1, backgroundColor: '#fff', shadowColor: '#000', shadowOffset: { width: 0, height: 3 }, shadowOpacity: 0.2, elevation: 5, padding: 10 },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingBottom: 10
+    },
+    imageProduct: {
+        height: heightImageProduct,
+        width: widthImageProduct,
+        marginHorizontal: 5
+    },
+    wrapperImage: {
+        flexDirection: 'row'
+    },
+    wrapperTitleProduct: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 10
+    },
+    txtProductName: {
+        fontWeight: '800',
+        fontSize: 18
+    },
+    txtProductPrice: {
+        fontSize: 18,
+        color: '#a39a99'
+    },
+    txtProductDescription: {
+        color: '#a39a99',
+        marginTop: 20
+    },
+    wrapperInfo: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 10
+    },
+    txtInfo: {
+        color: '#e04696'
+    }
+});
